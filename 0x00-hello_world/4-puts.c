0d763df8a6be863c718bd3e0b5c34b8ee2371054
Temiloluwa16 <lotemi.b2020@gmail.com>
2022-07-03 10:44:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * main-prints the size of various types
- * Return:0 if exited propely, non-zero otherwise
+ * main - Entry point
+ *
+ * Return: Always 0
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-} 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+
+/**
+ * main - Entry points
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof (char));
+	printf("Size of an int: %d byte(s)\n", sizeof (int));
+	printf("Size of
