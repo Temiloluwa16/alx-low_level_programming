5d721abd133f5f4522813a3e3c24bb33587f4e59
Temiloluwa16 <lotemi.b2020@gmail.com>
2022-07-03 08:43:10 -0700

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints the size of various types
+ * Return:0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
-return(0);
-} 
+	puts("\"Programming is like building a multilingual puzzle, followed by a new line");
+return (0);
+}
