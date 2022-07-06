77f99c3cbfa4ee0ae5a8ff9b340c40d468f7a719
Temiloluwa16 <lotemi.b2020@gmail.com>
2022-07-06 09:26:30 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main-> assign a random number to the variable n each time it is executed
  * based a condition
- * Return: 0
+ * Return: always 0
  */
 int main(void)
 {
@@ -22,3 +22,4 @@ if (n < 0)
 	printf("%d is negative\n", n);
 }
 return (0);
+}
