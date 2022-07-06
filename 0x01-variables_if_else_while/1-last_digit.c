63d53f3f771d3240ad847fcb2ab78f9c640727dd
Temiloluwa16 <lotemi.b2020@gmail.com>
2022-07-06 11:07:01 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,11 +13,17 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf("last digit of %d is ", n);
 if (n > 5)
+{
 	printf("greater than 5");
+}
 if (n == 0)
+{
 	printf("0");
+}
 if (n < 0 && n != 0)
+{
 	printf("less than 6 not 0");
-printf(" \n");
+}
+printf("\n");
 return (0);
 }
