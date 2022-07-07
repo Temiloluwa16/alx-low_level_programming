7c5fe6f33df59254d6c30d3d5c372f6126f9dd0e
Temiloluwa16 <lotemi.b2020@gmail.com>
2022-07-06 17:24:43 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main-> assign a random number to the variable n each time
+ * it is executed and prints out
+ * Return: Always 0
+ */
+int main(void)
+{
+int ch;
+
+for (ch = 48; ch <= 57; ch++)
+{
+putchar(ch);
+}
+
+putchar(10);
+return (0);
+}
+
