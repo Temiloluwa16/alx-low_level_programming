f8fd397b6a8a533d23180d759602d94d87259feb
Temiloluwa16 <lotemi.b2020@gmail.com>
2022-07-08 15:55:41 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: alwats return 0
+ */
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_neative(i);
+return (0);
+}
