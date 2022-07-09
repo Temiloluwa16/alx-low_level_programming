2595e09492aea6b8572c6a918aff6860dea02df8
Temiloluwa16 <lotemi.b2020@gmail.com>
2022-07-09 15:35:48 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main- prints putchar
+ * Return: always return 0
+ */
+int main(void)
+{
+putchar("_");
+putchar("p");
+putchar("u");
+putchar("t");
+putchar("c");
+putchar("h");
+putchar("a");
+putchar("r");
+putchar("\n");
+return (0);
+}
