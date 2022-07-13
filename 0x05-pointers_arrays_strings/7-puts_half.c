5ddd586c59e4b8eb29d515ed3fe44a9fceab70a1
Temiloluwa16 <lotemi.b2020@gmail.com>
2022-07-13 13:35:34 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half -> puts half
+ * @str: string parem
+ */
+void puts_half(char *str)
+{
+int x, y, i;
+x = strlen(str);
+if (x % 2 == 1)
+y = x / 2 + 1;
+else
+y = x / 2;
+for (i = y; i < ; i++)
+_putchar(str([i]);
+_putchar('\n');
