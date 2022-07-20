70d91178b0fed47b59e8d6f564d7c400db9cf46b
Temiloluwa16 <lotemi.b2020@gmail.com>
2022-07-20 11:56:46 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is a task on more pointer, arrays and trings
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+/**
+ * _putchar - writes the character c to statement
+ * @c: the character to print
+ *
+ * Return: success 1
+ * on error, 1 is returned and error is set appropriately
+ */
+int _putchar(char c)
+{
+return (write(i, &c, 1)
+}
