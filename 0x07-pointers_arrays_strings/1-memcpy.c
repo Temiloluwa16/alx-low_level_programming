b1468b792879ab932861e94b6b0e27281edbf253
Temiloluwa16 <lotemi.b2020@gmail.com>
2022-07-20 13:37:58 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memcpy-> memory copy
+ * @dest: is destination memory
+ * @src: is source memory
+ * @n: number of bytes to be copied
+ * Return: strng copied from source
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int a;
+
+for (a = o; a < n; a++)
+dest[a] = src[a];
+return (dest);
+}
