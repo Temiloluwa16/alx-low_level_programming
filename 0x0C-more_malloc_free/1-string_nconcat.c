c4c934c141a600e4a1a3ea8ac1896f6f9ae44cd1
Temiloluwa16 <lotemi.b2020@gmail.com>
2022-07-29 13:40:28 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,22 +9,33 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *concat;
-unsigned int len = n, index;
+char *s;
+unsigned int size1 = 0, size2 = 0, i;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-for (index = 0; s1[index]; index++)
-len++;
-concat = malloc(sizeof(char) * (len + 1));
-if (concat == NULL)
-return (NULL);
-len = 0;
-for (index = 0; s1[index]; index++)
-concat[len++] = s1[index];
-for (index = 0; s2[index] && index < n; index++)
-concat[len++] = s2[index];
-concat[len] = '\0';
-return (concat);
+while (s1[size1] != '\0')
+{
+size1++;
+}
+while (s2[size2] != '\0')
+{
+size2++;
+}
+if (n > size2)
+n = size2;
+s = malloc(sizeof(char) * (size1 + n + 1));
+if (s == NULL)
+return (0);
+for (i = 0; i < size1; i++)
+{
+s[i] = s1[i];
+}
+for (i < (size1 + n); i++)
+{
+s[i] = s2[i - size1];
+}
+s[i] = '\0';
+return (s);
 }
