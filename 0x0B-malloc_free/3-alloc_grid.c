9ac30989f324200d930a8bdd25cfbec0353e019c
Temiloluwa16 <lotemi.b2020@gmail.com>
2022-07-31 11:37:35 -0700

    hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,6 +14,7 @@ if (width <= 0 || height <= 0)
 return (NULL);
 arr = malloc(sizeof(int *) * height);
 if (arr == 0)
+return (NULL);
 for (i = 0; i < height; i++)
 {
 arr[i] = (malloc(sizeof(int) * width));
