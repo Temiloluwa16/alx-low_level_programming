6be850e597bb89e382c2a199173ae7bf50b290e8
Temiloluwa16 <lotemi.b2020@gmail.com>
2022-08-07 08:50:14 -0700

    hot fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,7 +6,7 @@
  * Desc: Header file containing prototypes for all functions
  * used in the 0x0F-variadic_functions directory
  */
-#include (stdarg.h>
+#include <stdarg.h>
 /**
  * struct printer - A new struct type defining a printer
  * @symbol: A symbol representing a data type
